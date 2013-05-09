9f89bb73e535065e2e471974eb234a37df96b81e
Chris Kuehl <chris@techxonline.net>
2013-05-09 11:59:00 -0400

    Validate name upon entry.

--- client/www/screens/distro/start/start.js
+++ client/www/screens/distro/start/start.js
@@ -5,6 +5,18 @@ gui.screens["distro/start"].data = {
 	}],
 
 	setup: function(contentManager) {
-		
+		$(".continue").click(function() {
+			var name = $(".fullName").val().trim();
+			
+			if (name.length < 3) {
+				return dialog("Invalid Name", "Please use a valid name.", ["Sorry!"]);
+            }
+            
+            if (name.indexOf(" ") <= (- 1)) {
+				return dialog("Full Name Required", "Please use your full name.", ["Sorry!"]);
+            }
+            
+            return alert("ok");
+		});
 	}
 };
